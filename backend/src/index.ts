b81dcc340625e2bf3e389cc9df96b26eec445721
giucino <125276491+giucino@users.noreply.github.com>
2024-12-09 16:13:49 +0100

    Implemented Error Handling in express.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,19 @@
 import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
 dotenv.config({
   path: "./src/.env",
 });
-import express from "express";
+
+import "express-async-errors";
 import "./database/index";
 import categoryRoutes from "./routes/category.routes";
 import tagRoutes from "./routes/tag.routes";
 import postRoutes from "./routes/post.routes";
 import commentRoutes from "./routes/comment.routes";
 import authRoutes from "./routes/auth.routes";
+import logger from "./shared/logger.util";
+import { Category } from "./models/Category";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,11 +25,27 @@ app.use(express.urlencoded({ extended: true }));
 //     res.json((req as any).user);
 //   });
 
+// app.get("/", async (req, res) => {
+//   // @ts-ignore
+//   await Category.findAlll();
+//   res.send("Hello World!");
+// });
+
 app.use("/api/categories", categoryRoutes);
 app.use("/api/tags", tagRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/auth", authRoutes);
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: any) => {
+  logger.error({
+    message: err.message,
+    stack: err.stack,
+  });
+
+  res.status(500).send("Something went wrong");
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
---
+++ backend/src/shared/logger.util.ts
@@ -0,0 +1,18 @@
+import { createLogger, format, transports } from "winston";
+
+const logger = createLogger({
+  level: "error",
+  format: format.combine(
+    format.timestamp(),
+    format.json(),
+    format.printf(({ level, message, timestamp, stack }) => {
+      return `${timestamp} ${level}: ${message} | ${stack || ""}`;
+    })
+  ),
+  transports: [
+    new transports.File({ filename: "error.log" }),
+    new transports.Console(),
+  ],
+});
+
+export default logger;
