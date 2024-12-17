ba0e8522e2a084dc8ce25680d017e47053717abd
giucino <125276491+giucino@users.noreply.github.com>
2024-12-18 00:49:41 +0100

    Admin Panel Setup - Angular Material and Navigation

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    FooterComponent,
+    MatSidenavModule,
+    SidebarComponent
+  ],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss',
+  styleUrl: './admin.component.scss'
 })
-export class AdminComponent {}
+export class AdminComponent {
+
+}
--- frontend/src/app/admin/core/components/header/header.component.ts
+++ frontend/src/app/admin/core/components/header/header.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Output() toggleBtnClicked = new EventEmitter<void>();
+  authService = inject(AuthService)
 
 }
