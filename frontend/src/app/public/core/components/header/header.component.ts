baa4388586050fa10d7c1f0cc0ebc30d06f5fe3c
giucino <125276491+giucino@users.noreply.github.com>
2024-12-31 23:49:12 +0100

    Refactor public component and header component styles

--- frontend/src/app/public/core/components/header/header.component.ts
+++ frontend/src/app/public/core/components/header/header.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../../../../core/services/auth.service';
+import { DarkModeService } from '../../../../core/services/dark-mode.service';
 
 @Component({
   selector: 'app-header',
@@ -13,6 +15,8 @@ import { AuthService } from '../../../../core/services/auth.service';
 export class HeaderComponent {
   authService = inject(AuthService);
   isMenuOpen = false;
+  private darkModeService = inject(DarkModeService);
+  isDarkMode$: Observable<boolean> = this.darkModeService.isDarkMode();
 
   menuItems = [
     { path: '/', label: 'Home' },
@@ -20,7 +24,25 @@ export class HeaderComponent {
     { path: '/contact', label: 'Contact' },
   ];
 
+  constructor(private router: Router) {}
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  /**
+   * Toggle the dark mode. This will update the document body class and store the setting in local storage.
+   */
+  toggleDarkMode() {
+    this.darkModeService.toggleDarkMode();
+  }
+
+  isActive(path: string): boolean {
+    return this.router.isActive(path, {
+      paths: 'exact',
+      queryParams: 'exact',
+      fragment: 'ignored',
+      matrixParams: 'ignored',
+    });
   }
 }
