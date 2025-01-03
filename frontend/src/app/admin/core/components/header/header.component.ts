a38d9d0221abc3dbc902e700ba44c229b9106de5
giucino <125276491+giucino@users.noreply.github.com>
2025-01-03 16:09:04 +0100

    Refactor footer and header components

--- frontend/src/app/admin/core/components/header/header.component.ts
+++ frontend/src/app/admin/core/components/header/header.component.ts
@@ -2,21 +2,17 @@ import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
-  ],
+  imports: [RouterModule, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   @Output() toggleBtnClicked = new EventEmitter<void>();
-  authService = inject(AuthService)
-
+  authService = inject(AuthService);
 }
--- frontend/src/app/public/core/components/footer/footer.component.ts
+++ frontend/src/app/public/core/components/footer/footer.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   menuItems = [
@@ -16,16 +16,5 @@ export class FooterComponent {
     { path: '/contact', label: 'Contact' },
   ];
 
-  constructor(private router: Router) {}
-
-
-  isActive(path: string): boolean {
-    return this.router.isActive(path, {
-      paths: 'exact',
-      queryParams: 'exact',
-      fragment: 'ignored',
-      matrixParams: 'ignored',
-    });
-  }
-
+  constructor() {}
 }
